ee9d853f5c39fd8bf479eefc86cee96c59907c49
Q-b1t <darioluishinojosaq@mail.com>
2023-09-26 10:59:29 -0600

    make request directly from api

--- dehashed_dump_processing.py
+++ dehashed_dump_processing.py
@@ -5,6 +5,21 @@ from termcolor import colored
 from argparse import ArgumentParser,Namespace
 import os
 import matplotlib.pyplot as plt
+import requests
+import configparser
+import sys
+
+def make_dehashed_request(mail,api_key,num_pages,num_requests,domain):
+    headers = {
+        'Accept': 'application/json',
+    }
+    response = requests.get(
+        f'https://api.dehashed.com/search?query=domain:{domain}&size={num_requests}&page={num_pages}',
+        headers=headers,
+        auth=(mail, api_key),
+    )
+    return response
+
 
 def read_data(dump_file):
     """
@@ -65,20 +80,39 @@ def generate_insights(parsed_table):
 if __name__ == '__main__':
     # instance command line parser
     parser = ArgumentParser()
-    parser.add_argument("-i","--input_file",help="Name json file dumped using the DEHASHED API (default: \"breached_passwords.json\")",type=str,default="breached_passwords.json",nargs="?")
     parser.add_argument("-o","--output_file",help="Name of excel book the data will be exported to (default: \"dehashed_dump.xlsx\")",type=str,default="dehashed_dump.xlsx",nargs="?")
     parser.add_argument("-n","--null_value",help="Specify a different null value to fill the blank spaces (default: np.nan)",type=str,default=np.nan,nargs="?")
     parser.add_argument("-g","--insights",help="Whether to generate graphs used in reports (default: np.nan)",type=bool,default=False,nargs="?")
+    parser.add_argument("-c","--config_file",help="Configuration file contianing the API keys (default: \"conf.cfg\")",type=str,default="conf.cfg",nargs="?")
+    parser.add_argument("-s","--size",help="The number of requests to be made in the Dehashed API (view API documentation)",type=int,default=1000,nargs="?")
+    parser.add_argument("-p","--pages",help="Configuration file contianing the API keys (default: \"conf.cfg\")",type=int,default=1,nargs="?")
+    parser.add_argument("-d","--domain",help="The mail domain to make searches on (default: \"dehashed.com\")",type=str,default="dehashed.com",nargs="?")
 
+
+    # retrieve cli arguments
     args: Namespace = parser.parse_args()
-    dump_file = args.input_file
     export_file = args.output_file
+    config_file = args.config_file
     na_value = args.null_value
     insights = args.insights
+    size = args.size 
+    pages = args.pages
+    domain = args.domain
 
-    
-    # load the json data as a python dictionary
-    json_data = json.loads(read_data(dump_file=dump_file))
+    # get configuration values
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    assert config['API_KEY']['dehashed'] != "" or config['API_KEY']['email'] != "", colored("[-] The configuration file does not contain an API key, email or the format is not the appropiate one.","red")
+
+    DEHASHED_API_KEY = config['API_KEY']['dehashed']
+    VERIFICATION_MAIL = config['API_KEY']['email']
+
+    response = make_dehashed_request(mail=VERIFICATION_MAIL,api_key=DEHASHED_API_KEY,num_requests=size,num_pages=pages,domain=domain)
+    if response.status_code == 200:
+        json_data = json.loads(response.text)
+    else:
+        print(colored("Response was not successful","red"))
+        sys.exit()
 
     # extract usefull metadata from the dump
     num_entries = json_data["total"]
